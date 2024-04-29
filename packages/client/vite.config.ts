7a25912c5fd48e566b1a0fdda6da36ee37f5663a
Can Delibas <candelibas96@gmail.com>
2024-04-29 22:18:01 +0300

    update vite config from js to ts

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  return defineConfig({
+    envDir: "../../",
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            phaser: ["phaser"],
+          },
+        },
+      },
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://localhost:3001",
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+      hmr: {
+        clientPort: 3000, // for dev: 3000, for production: 443
+      },
+    },
+  });
+};
