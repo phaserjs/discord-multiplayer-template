de41211e5da5c49489c31d0d7f7bf1950fac24ac
Can Delibas <candelibas96@gmail.com>
2024-05-10 18:25:06 +0300

    implement discord sdk mockup

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,6 +1,5 @@
-import { DiscordSDK } from "@discord/embedded-app-sdk";
-
 import { ScaleFlow } from "./utils/ScaleFlow";
+import { initiateDiscordSDK, discordSdk } from "./utils/discordSDK";
 
 import { Boot } from "./scenes/Boot";
 import { Game } from "./scenes/Game";
@@ -8,21 +7,9 @@ import { MainMenu } from "./scenes/MainMenu";
 import { Preloader } from "./scenes/Preloader";
 import { Background } from "./scenes/Background";
 
-// Instantiate the SDK
-// Uncomment this once your work is done on browser, it will ONLY work on Discord Activities
-/* const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
-setupDiscordSdk()
-  .then(() => {
-    console.log("Discord SDK is ready");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-async function setupDiscordSdk() {
-  await discordSdk.ready();
-} */
-
 (async () => {
+  initiateDiscordSDK();
+
   new ScaleFlow({
     type: Phaser.AUTO,
     parent: "gameParent",
