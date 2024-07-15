d0235a0a1a7ad293c4407cce3a573525c7eefce9
Can Delibas <candelibas96@gmail.com>
2024-07-15 20:13:46 +0300

    fix nodemon script for windows

--- packages/server/dev-command.js
+++ packages/server/dev-command.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 const os = require('os');
 
 if (os.platform() === 'win32') {
-    execSync(`nodemon --watch src -e ts,ejs --exec \"node %npm_execpath% start\"`, { stdio: 'inherit', shell: true });
+    execSync(`nodemon --watch src -e ts,ejs --exec %npm_execpath% start`, { stdio: 'inherit', shell: true });
 }
 else {
     execSync(`nodemon --watch src -e ts,ejs --exec $npm_execpath start`, { stdio: 'inherit' });
